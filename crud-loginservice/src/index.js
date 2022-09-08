19e63b54aa1b904aaeefde4bc7264fa3829c84e0
ConradMar3 <conradmare890317@gmail.com>
2022-09-08 14:33:33 +0200

    V2

--- crud-loginservice/src/index.js
+++ crud-loginservice/src/index.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { initializeApp } from "firebase/app";
+import registerServiceWorker from './registerServiceWorker';
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDizWE09zgmoWqL8O7TV-smEf-M6bEm2RY",
-  authDomain: "react-loginservice.firebaseapp.com",
-  projectId: "react-loginservice",
-  storageBucket: "react-loginservice.appspot.com",
-  messagingSenderId: "826387399783",
-  appId: "1:826387399783:web:36c217febca05c494400c2"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
